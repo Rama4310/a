dc2787998c5e517eb91b3a00ce2374f546ca3204
rama <ramarao4310@gmail.com>
2024-12-07 08:34:49 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,15 @@
+// Array print the input using index value
+public class Array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []int2 = {10, 20, 30, 40, 50};
+		System.out.println(int2[0]);
+		System.out.println(int2[1]);
+		System.out.println(int2[2]);
+		System.out.println(int2[3]);
+		System.out.println(int2[4]);
+
+	}
+
+}
